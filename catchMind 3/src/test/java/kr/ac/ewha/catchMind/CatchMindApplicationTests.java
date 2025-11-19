715b82be0b33d090ca31391c25b2fbf180439888
Minkyoung Lee <minkyoungemilylee@ewha.ac.kr>
2025-11-19 21:04:53 +0900

    create project

---
+++ catchMind 3/src/test/java/kr/ac/ewha/catchMind/CatchMindApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ewha.catchMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
